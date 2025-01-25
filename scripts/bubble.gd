3b9524ee774f669e9fd2038d243c5701354db97f
JermuWallu <jere.wahlsten@gmail.com>
2025-01-25 12:44:13 +0200

    jeejee bullet bubble toimii

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,13 +1,32 @@
-extends Node
+extends CharacterBody2D
 
+signal on_player_entered
+
+@export var speed = 200
+
+# gets called in player.gd
+func start(_position, _direction):
+	rotation = _direction
+	position = _position
+	velocity = Vector2(speed, 0).rotated(rotation)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if collision:
-	print("I collided with ", collision.get_collider().name)
 	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	print("i was touched by", body)
+	if body.name == "Player":
+		on_player_entered.emit()
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+
+func _on_VisibilityNotifier2D_screen_exited():
+	# Deletes the bullet when it exits the screen.
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,27 +4,51 @@ extends CharacterBody2D
 @export var jump_speed = -400.0
 var bubbleTexture
 var isInBubble = false
+var Bubble = preload("res://scenes/bubble.tscn")
+var bubbleSpeed = 200
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	# Add the gravity.
-	velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = jump_speed
-
+func get_input():
+	
 	# Get the input direction.
 	var direction = Input.get_axis("left", "right")
 	velocity.x = direction * speed
+	
+	# Handle Jump.
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jump_speed
+	
+	# shoot input
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	pass
 
+func shoot():
+	var b = Bubble.instantiate()
+	b.start($marker.global_position, rotation)
+	get_tree().root.add_child(b)
+	pass
+
+func _physics_process(delta):
+	get_input()	
+	
+	# inside and outside bubble gravity
 	if isInBubble:
 		velocity.y = -speed
-
+	else:
+		velocity.y += gravity * delta
+		
+	
 	move_and_slide()
-
+	
+	# collision 
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+#		print("Collided with: ", collision.get_collider().name)
+	pass
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bubbleTexture = find_child("BubbleTexture")
@@ -48,3 +72,11 @@ func on_new_level_entry() -> void:
 func on_new_level_entered() -> void:
 	self.set_collision_layer_value(3, true)
 	self.set_collision_mask_value(3, true)
+		
+		
+	pass
+
+
+func _on_bubble_on_player_entered() -> void:
+	print("entered bubble")
+	pass # Replace with function body.
