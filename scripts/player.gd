f2f6b06ede148abee9bc1ab55bfb23af0df38a56
Frans Paasonen <franspaasonen@gmail.com>
2025-01-25 21:15:12 +0200

    Player animations maybe workerino

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,10 @@ class_name Player extends CharacterBody2D
 
 signal player_entering_new_level(levelPos: Vector2)
 
+@onready var animationPlayer = $AnimationPlayer
+
+@onready var sprite = $playerTexture
+
 @export var speed = 300.0
 @export var jump_speed = -400.0
 var bubbleTexture
@@ -18,6 +22,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func bubbleHit():
 	print("Player got hit by a bubble.")
 	isInBubble = true
+	animationPlayer.play("bubble")
 	await get_tree().create_timer(1.0).timeout
 	isInBubble = false
 	hasBubble = true
@@ -28,9 +33,11 @@ func spawnBubble():
 	var b = Bubble.instantiate()
 	b.start($marker.global_position, rotation)
 	get_tree().root.add_child(b)
+	animationPlayer.play("blowBubble")
 	pass
 
 func kick():
+	animationPlayer.play("punch")
 	var collisions = $KickArea.get_overlapping_bodies()
 	for i in collisions:
 		if i is Bubble:
@@ -41,22 +48,34 @@ func kick():
 
 func get_input():
 	if Input.is_action_just_pressed("kick"):
-		print("SUPER KICK") 
+		print("SUPER KICK")
 		kick()
-	
-	
+
+
 	# Get the input direction.
 	var direction = Input.get_axis("left", "right")
 	velocity.x = direction * speed
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
+		animationPlayer.play("jump")
 		velocity.y = jump_speed
 
 	# shoot input
 	if Input.is_action_just_pressed("bubble") and hasBubble:
 		spawnBubble()
-	pass
+
+	# Handle animation state changing
+	var resetAnims = ["walk", "bubble"]
+	
+	if velocity.y > 0:
+		animationPlayer.play("bubble")
+	if direction != 0:
+		sprite.set_scale(Vector2(direction, 1))
+		if velocity.y == 0:
+			animationPlayer.play("walk")
+	elif direction == 0 and velocity.y == 0 and animationPlayer.is_playing() and resetAnims.find(animationPlayer.current_animation) != -1:
+		animationPlayer.play("RESET")
 
 func _physics_process(delta):
 	get_input()
@@ -76,7 +95,6 @@ func _physics_process(delta):
 		if collisionObject is Bubble and !isInBubble:
 			collisionObject.free()
 			bubbleHit()
-	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -89,24 +107,23 @@ func _process(_delta: float) -> void:
 		bubbleTexture.show()
 	else:
 		bubbleTexture.hide()
-	pass
 
 func on_new_level_entry(level: Level) -> void:
 	self.set_collision_layer_value(3, false)
 	self.set_collision_mask_value(3, false)
-	
+
 	player_entering_new_level.emit(level.position)
 
 func on_new_level_entered(level: Level) -> void:
 	self.set_collision_layer_value(3, true)
 	self.set_collision_mask_value(3, true)
-	
+
 	if self.currentLevel:
 		self.currentLevel.set_monitor_entry(true)
 
 	level.set_monitor_entry(false)
 	self.currentLevel = level
-	
+
 
 func _on_bubble_on_player_entered() -> void:
 	print("entered bubble")
