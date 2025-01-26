7ee6db4aa1f6b575e092a029d4c27ee25f7d0b88
JermuWallu <jere.wahlsten@gmail.com>
2025-01-26 14:46:27 +0200

    refined player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,8 @@ signal player_entering_new_level(levelPos: Vector2)
 @export var speed = 300.0
 @export var jump_speed = -400.0
 @export var mass: float = 1.00 # abstract value to dampen the floatiness in jump 
+@export var acceleration = 600
+@export var deceleration = 800
 
 var walkAudioTimer = 0
 
@@ -68,16 +70,21 @@ func checkBubbleHit() -> void:
 			if collider is Bubble:
 				collider.getKicked()
 
-func handleInput():
+func handleInput(delta):
 	if Input.is_action_just_pressed("kick"):
 		print("SUPER KICK")
 		animationPlayer.play("punch")
 		audio.playPunch()
 		checkBubbleHit()
 
-	# Get the input direction.
-	var direction = Input.get_axis("left", "right")
-	velocity.x = direction * speed
+	# Handle horizontal movement
+	if Input.is_action_pressed("left"):
+		velocity.x = move_toward(velocity.x, -speed, acceleration * delta)
+	elif Input.is_action_pressed("right"):
+		velocity.x = move_toward(velocity.x, speed, acceleration * delta)
+	else:
+		# Decelerate when no input
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
@@ -92,6 +99,7 @@ func handleInput():
 	# Handle animation state changing
 	var resetAnims = ["walk", "bubble"]
 	
+	var direction = Input.get_axis("left","right")
 	if velocity.y > 0:
 		animationPlayer.play("bubble")
 	if direction != 0:
@@ -104,7 +112,7 @@ func handleInput():
 		animationPlayer.play("RESET")
 
 func _physics_process(delta):
-	handleInput()
+	handleInput(delta)
 
 	# inside and outside bubble gravity
 	if isInBubble:
